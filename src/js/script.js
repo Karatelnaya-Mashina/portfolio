e46cc1b7be556b6cbe37e19cf60ef6052a8e51c6
Karatel <karatel.kara1990@gmail.com>
2024-09-23 09:45:09 +0300

    vers. 0.9.0

--- src/js/script.js
+++ src/js/script.js
@@ -16,3 +16,21 @@ const counters = document.querySelectorAll('.skills__percentages'),
 counters.forEach((item, i) => {
 	strip[i].style.width = item.innerHTML
 })
+
+$(document).ready(function () {
+	// Smooth scroll and pageup
+
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 1600) {
+			$('.pageup').fadeIn()
+		} else {
+			$('.pageup').fadeOut()
+		}
+	})
+	$('a[href=#up]').click(function () {
+		const _href = $(this).attr('href')
+		$('html, body').animate({ scrollTop: $(_href).offset().top + 'px' })
+		return false
+	})
+	new WOW().init()
+})
